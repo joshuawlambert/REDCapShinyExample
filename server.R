b60cb8e36acfa4d6c67a9c26309327a60cecce5e
Joshua Lambert <jmsuk0587@gmail.com>
2019-06-28 10:12:40 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,35 @@
+#redcap
+library(REDCapR)
+library(shiny)
+library(shinyBS)
+source('/home/MC/jwla225/ShinyApps/api_token_testREDCap.R',local=T)
+
+shinyServer(function(input, output) {
+  ds <<- redcap_read(redcap_uri=uri, token=token)$data
+  
+  # a large table, reative to input$show_vars
+  output$redcaptable = renderDataTable({
+    ds <<- redcap_read(redcap_uri=uri, token=token)$data
+    ds
+  })
+  
+  observeEvent(input$upload,{
+    upds<-data.frame(matrix(c(max(ds$record_id)+1,input$firstname,input$lastname,input$strcitystname,
+            input$phone,input$email,as.character(input$dob),as.numeric(floor(difftime(Sys.time(),as.character(input$dob))/365)),input$ethn,input$race,
+            input$gen,input$ht,input$wt,((input$wt)/(input$ht*input$ht))*10000,input$comments),nrow=1),2)
+    colnames(upds)<-colnames(data.frame(ds))
+    print(as.numeric(floor(difftime(Sys.time(),as.character(input$dob))/365)))
+    redcap_write(ds=upds,redcap_uri=uri,token=token)
+    Sys.sleep(2)
+    ds <<- redcap_read(redcap_uri=uri, token=token)$data
+    })
+  
+  output$heighthist=renderPlot({
+    hist(ds$height)
+  })
+  output$agehist=renderPlot({
+    hist(ds$age)
+  })
+})
+
+
